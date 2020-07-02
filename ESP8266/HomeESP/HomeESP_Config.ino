2f28b3c36af38f310fdac9e10ca4943da8a96d85
Jeffrey <9938813+Raphire@users.noreply.github.com>
2020-07-02 21:19:07 +0200

    Added loadFile and saveConfig

--- ESP8266/HomeESP/HomeESP_Config.ino
+++ ESP8266/HomeESP/HomeESP_Config.ino
@@ -23,14 +23,14 @@ bool createHomeESPConfig(const char* filename,
   jsonBuffer["kakuAddress"] = _kakuAddress;
   jsonBuffer["kakuUnits"] = _kakuUnits;
 
-  return ESPHelperFS::saveConfig(jsonBuffer, filename);
+  return saveConfig(jsonBuffer, filename);
 }
 
 
 int8_t validateHomeESPConfig(const char* filename) {
   StaticJsonDocument<JSON_SIZE> jsonBuffer;
   
-  if(!ESPHelperFS::loadFile(filename, &jsonBuffer)) {
+  if(!loadFile(filename, &jsonBuffer)) {
     return CANNOT_PARSE;
   }
   
@@ -52,4 +52,52 @@ int8_t validateHomeESPConfig(const char* filename) {
     return INCOMPLETE;
   }
   return GOOD_CONFIG;
+}
+
+bool loadFile(const char* filename, JsonDocument* buffer){
+  // Open file as read only
+  File configFile = SPIFFS.open(filename, "r");
+
+  // Check to make sure opening was possible
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    configFile.close();
+    return false;
+  }
+
+  // Make sure the config isnt too large to store in the JSON container
+  size_t size = configFile.size();
+  FSdebugPrint("JSON File Size: ");
+  FSdebugPrintln(size);
+  if (size > JSON_SIZE) {
+    Serial.println("JSON File too large");
+    return false;
+  }
+
+  DeserializationError error = deserializeJson(*buffer, configFile);
+  if(error && size != 0){
+      Serial.println("JSON File corrupt/could not be deserialized");
+      return false;
+  }
+
+  // Close out the file and return true
+  configFile.close();
+  return true;
+}
+
+bool saveConfig(JsonDocument& json, const char* filename) {
+  if(SPIFFS.exists(filename)){
+    SPIFFS.remove(filename);
+  }
+
+  File configFile = SPIFFS.open(filename, "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  serializeJson(json, configFile);
+
+  configFile.close();
+  return true;
 }
